e98e6c500fe752039a95bf06455857c70493040e
Reagan lung <ragquan@Reagans-MacBook-Air.local>
2024-09-09 10:45:26 -0700

    colour change

--- test.js
+++ test.js
@@ -2,3 +2,5 @@ console.log("Welcome to the amazing Metal Person");
 console.log("TEST")
 console.log("im going to skip legs again")
 console.log"(im hitting chest again")
+
+console.log("\x1b[23m", "Hi")
