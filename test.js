3b1d661dc53e7f0eac30fedf0a04c62d741cc10f
Reagan lung <ragquan@Reagans-MacBook-Air.local>
2024-09-09 11:03:53 -0700

    colour change

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 console.log("Welcome to the amazing Metal Person");
-console.log("TEST")
-console.log("im going to skip legs again")
-console.log"(im hitting chest again")
+console.log("TEST");
+console.log("im going to skip legs again");
+console.log("im hitting chest again");
 
-console.log("\x1b[35m", "Hi")
+console.log("\x1b[35m", "Hi");
 
-console.log (x1b[om]
+console.log("x1b[om]");
