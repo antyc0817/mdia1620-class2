272ebea5fa174b562b1b2131682119fa716c5d59
Reagan lung <ragquan@Reagans-MacBook-Air.local>
2024-09-16 11:18:41 -0700

    Reagan Const practioce

--- test.js
+++ test.js
@@ -1,20 +1,33 @@
-console.log("Welcome to the amazing Metal Person");
-console.log("TEST");
-console.log("im going to skip legs again");
-console.log("im hitting chest again");
-
-console.log("\x1b[35m", "Hi");
-
-console.log("x1b[om]");
-console.log("\x1b[34m]");
-
-console.log("\x1b[36m");
-console.log('\x1b[36m%s\x1b[0m', 'I am cyan'); 
-
-console.log ("\x1b[0m");
-console.log ("\x1b[35m");
-
-onsole.log('\x1b[35m%s\x1b[0m', 'Meow Meow Meow');
 
 
+const gym="workout";
 
+let excersice="chest workout"
+
+console.log("Today i am going to "+gym+" at BCIT. I try to go 4 times a week. My favourite workout is "+excersice+". Hopefully I don't have to wait for a bench today")
+
+excersice="back workout"
+
+console.log("===================================")
+
+console.log("Yesterday, the gym was very busy but I was able to do a "+excersice+" as the machines was free. I went to the gym at " , 9 ,"pm as it wasn't too busy")
+
+console.log("=================================")
+
+excersice="leg workout"
+
+console.log("When i typically do my "+gym+" i always hate "+excersice+". If I feel tired, i would skip my "+excersice+" since it is not fun")
+
+console.log("=================================")
+
+console.log(`My favourite place to ${gym} is at goodlife. I don't like to gym around` , 6 , `pm as that is when it is the most packed`)
+
+/* 
+
+Hi Regan, I like your code, and organization. the only thing I think could be worth adjusting is the spacing when declaring and using vairables.
+
+-Diego B
+*/
+/* hi Regan, all looks good but I would consider make the spacing a little bit more organized to make it look clear.
+Saahil
+*/ 
