6508b945ab58dc60b177b8be3ea291028205668e
Reagan lung <ragquan@Reagans-MacBook-Air.local>
2024-10-20 04:43:21 -0700

    m

--- practice.js
+++ practice.js
@@ -12,3 +12,24 @@ food="dog"
 console.log( "Welcome back mister " + kName + " I'm just using the other method. I will eat " + food + " since i so hungry")
 
 
+// only one of them has to be true, it will work since its OR
+
+condition1="1"
+condition2="2"
+
+if(true || false){
+  console.log("this will work since ")
+  }
+  
+  // AND both has to be true 
+  if (condition1 && true){
+    console.log("Kewl, this will work")
+  }
+  
+  
+  // this makes it so they have to be different 
+  // if i make condition 1 and 2 same, then it won't work since they have to be diff
+  if (condition2 !== condition1){
+    console.log("meow meow meow")
+  }
+  
