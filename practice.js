2ce70e52428acb01d740d29f5bd71ef92753ee43
Reagan lung <ragquan@Reagans-MacBook-Air.local>
2024-10-20 03:59:11 -0700

    const and let

--- practice.js
+++ practice.js
@@ -1,7 +1,14 @@
 
+const kName="David"
+let food="pizza"
+
+// its the small ` beside one for the ${}
+console.log(
+`Welcome back mister ${kName}, I'm just trying to make this easier to read. I'm going to eat ${food} now`
+)
+// this is harder to read but its the same thing at the end of the day. Just need the space in the quotation 
+// so the name would have the space
+food="dog"
+console.log( "Welcome back mister " + kName + " I'm just using the other method. I will eat " + food + " since i so hungry")
 
 
-
-switch(personName){
-  case
-}
